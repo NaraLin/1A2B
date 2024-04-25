367093c5bed1b02477336584dfe9d2ef57f9efc7
Nara <puniyoyo@gmail.com>
2024-04-26 07:29:00 +0800

    Initial Commit

---
+++ 2A1B/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2A1B
+//
+//  Created by 林靖芳 on 2024/4/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
